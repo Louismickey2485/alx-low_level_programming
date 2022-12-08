c68472f6fa85c4b72df2975ac50e667932c76677
Mikiyas Beyene <mikiyasmebratu5@gmail.com>
2022-12-08 11:50:10 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio .h>
+#include <stdio.h>
+
 /**
- * main - entry point
- * Return: Always 0 (success)
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
